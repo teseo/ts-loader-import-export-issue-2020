81bd3832a494aa8717114a9679e92d7b9463b92b
Javier Mellado <javier.mellado@paddypowerbetfair.com>
2020-05-18 17:39:42 +0100

    add complex interface

--- src/PageInterfaceA.tsx
+++ src/PageInterfaceA.tsx
@@ -1,7 +1,13 @@
- interface PageInterface {
-    color: string;
-  }
-
+interface PageInterface {
+  color: string
+  func?: (paramA: any, paramB: any) => void;
+  setParamB?: (key: string,
+    val1: string,
+    val2: string) => void;
+  setParamC?: (key: string,
+  val: string,
+  uniqueId: string) => void;
+}
 export { PageInterface }
 
 export default {}
